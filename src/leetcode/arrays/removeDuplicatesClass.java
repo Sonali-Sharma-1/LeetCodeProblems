c774478091fcc51f3703192067f3f01166ccdaf8
Sonali Sharma <sonali.sharma.bigbytes@gmail.com>
2020-10-03 13:43:40 +0530

    27. Remove Element

---
+++ src/leetcode/arrays/removeDuplicatesClass.java
@@ -0,0 +1,21 @@
+package leetcode.arrays;
+
+public class removeDuplicatesClass {
+    public int removeDuplicates(int[] nums) {
+        int currentIndex = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i+1]) {
+                nums[currentIndex++] = nums[i+1];
+            }
+        }
+        return currentIndex;
+    }
+
+
+    public static void main(String[] args) {
+        int[] input = {0,0,1,1,1,2,2,3,3,3,4};
+        removeDuplicatesClass object = new removeDuplicatesClass();
+        int output = object.removeDuplicates(input);
+        System.out.print(output);
+    }
+}
